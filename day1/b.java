793bfb57cef52780c6c4ee8a982a805498ab7da8
qka10777 <qka10777@gmail.com>
2023-08-31 09:37:32 +0900

    1일차 프로젝트 커밋

---
+++ day1/b.java
@@ -0,0 +1,24 @@
+package day1;
+
+public class b {
+    public static void main(String[] args) {
+
+        // int a = 5;
+        // int b = 10;
+
+        // System.out.println("a : " + a);
+        // System.out.println("b : " + b);
+
+        // 여기서 부터
+        int a = 10;
+        int b = 5;
+
+        // 여기까지 수정 가능
+
+        System.out.println("a : " + a);
+        // 출력 : a : 10
+        System.out.println("b : " + b);
+        // 출력 :  b : 5
+    }
+}
+
